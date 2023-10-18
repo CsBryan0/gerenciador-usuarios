b35c1158c65517e7e94028c67b5c42b9afdf94a5
Bryan Casotti <souzabryan09@gmail.com>
2023-10-18 11:25:53 -0300

    first commit

---
+++ src/main/java/com/comspringsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.comspringsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.comspringsecurity.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
